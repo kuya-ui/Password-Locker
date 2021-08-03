37c6d25dd138ecd453cde04afafb67aa90b59599
kuya-ui <kuyamaxmillan@gmail.com>
2021-08-03 22:57:44 +0300

    changes

--- user.py
+++ user.py
@@ -14,4 +14,43 @@ class User:
 
     def delete_detail():
 
-        
+        """
+        the delete_detail method is used to remove objects from the user detail array
+        """
+
+        User.user_detail.remove(self)
+
+    def __init__(self,account_name,username,password,confirm_password):
+
+        """
+        the __init__method helps us define properties for our objectsself.
+        """
+
+        self.account_name = account_name
+        self.username = username
+        self.password =password
+        self.confirm_password = confirm_password
+
+
+        """
+        arguments for our __init__method will include the following.
+        """
+
+    @classmethod
+    def display_all_details(cls):
+
+        return cls.user_detail
+
+    @classmethod
+    def find_by_username(cls,username):
+
+        for user in cls.user_detail:
+            if user.username == username:
+                return user
+
+    @classmethod
+    def user_exist(cls,username):
+
+        for user in cls.user_detail:
+            if user.username == username:
+                    return True
