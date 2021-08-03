30868b83f361a1f1a267efc13449e73fc811447c
kuya-ui <kuyamaxmillan@gmail.com>
2021-08-03 23:19:48 +0300

    test user

--- credentials.py
+++ credentials.py
@@ -5,4 +5,16 @@ class Credentials:
 
     credential_detail = []
 
-    
+    def __init__(self,credential_detail):
+
+        """
+        the __init__method helps us define properties for our objects.
+        """
+
+
+        self.credential_detail = credential_detail
+
+
+        """
+        arguments for our __init__method will include the following.
+        """
