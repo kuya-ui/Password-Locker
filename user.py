878dcf2e057ab5e261d6696a340e6cd77f680c96
kuya-ui <kuyamaxmillan@gmail.com>
2021-08-03 22:41:33 +0300

    changes

---
+++ user.py
@@ -0,0 +1,17 @@
+class User:
+
+    """
+    Class that will generate new instances of users
+    """
+    user_detail = []
+
+    def save_detail(self):
+
+        """
+        the method saves detail objects into the empty user_detail array
+        """
+        User.user_detail.append(self)
+
+    def delete_detail():
+
+        
