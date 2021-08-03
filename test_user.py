583c7ace7d434531e10a6d846b424db0956ce07b
kuya-ui <kuyamaxmillan@gmail.com>
2021-08-03 23:45:46 +0300

    changes

--- test_user.py
+++ test_user.py
@@ -29,3 +29,28 @@ def test_init(self):
     self.assertEqual(self.new_user.username,"millan")
     self.assertEqual(self.new_user.password,"abcd")
     self.assertEqual(self.new_user.confirm_password,"abcd")
+
+
+def test_save_detail(self):
+       """
+        the test_save_detail test is used to test if the contact object is saved into details
+        """
+       self.new_user.save_detail()
+
+       self.assertEqual(len(User.user_detail),1)
+
+def test_save_multiple_detail(self):
+
+        """
+        a method to check if we can save multiple user details to our details array
+        """
+def test_display_all_details():
+
+        """
+        is a method that returns a list of all details saved
+        """
+
+        self.assertEqual(User.display_all_details(),User.user_detail)
+
+if __name__ == '__main__':
+    unittest.main()
