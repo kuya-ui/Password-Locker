effc5fec7fddf465098c372ddd15940b0e50f6fc
kuya-ui <kuyamaxmillan@gmail.com>
2021-08-04 00:22:44 +0300

    changes

---
+++ run.py
@@ -0,0 +1,64 @@
+from user import User
+import getpass
+import random
+import string
+dash = '-' * 60
+
+def create_account(account_name,username,password,confirm_password):
+
+    """
+    function to create a new account
+    """
+
+    new_user = User(account_name,username,password,confirm_password)
+
+    return new_user
+
+def save_details(user):
+    
+    """
+    function to save save_details
+    """
+    user.save_detail()
+
+def display_all_details():
+
+    """
+    function used to return all saved save_details
+    """
+    return User.display_all_details()
+
+def check_existing_user(username):
+
+    """
+    a function that is used to check and return all existing accounts
+    """
+
+    return User.user_exist(username)
+
+def find_user(username):
+
+    """
+    the function is used check details from the saved save_details
+    """
+
+    return User.find_by_username(username)
+
+def generatePassword(num):
+   genpas = ''
+
+   for n in range(num):
+       x = random.randint(0,94)
+       genpas += string.printable[x]
+
+   return genpas
+
+def main():
+    print('{:_^5}'.format('RE-INVENT THE WAY YOU SAVE YOUR PASSWORDS WITH OUR PASSWORD LOCKER APP'))
+
+
+    print('\n')
+
+    print('{:_^20}'.format('login'))
+
+    print('\n')
